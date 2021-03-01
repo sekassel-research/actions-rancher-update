2f477a94b022ad13bf52a0c0ca35a118225324f8
Adrian Kunz <clashsoft@hotmail.com>
2021-03-01 14:19:21 +0100

    feat: Rancher 2 deployment

--- main.js
+++ main.js
@@ -1,75 +1,45 @@
-const core = require('@actions/core');
-const request = require('request-promise-native');
+import core from '@actions/core';
+import axios from 'axios';
 
 process.on('unhandledRejection', handleError);
 main().catch(handleError);
 
-const sleep = (sec) => new Promise((resolve) => setTimeout(resolve, sec * 1000));
-const waitForState = async (waitFor, rancherApi, id) => {
-  let retry = 10;
-  let state = '';
-  while (state !== waitFor && retry > 0) {
-    state = (await rancherApi.get(`/services/${id}`)).state;
-    retry--;
-    await sleep(5);
-  }
-
-  if (retry === 0) {
-    throw new Error(`Maximum retries exceeded waiting for state ${waitFor}`);
-  }
-}
-
 async function main() {
-  const RANCHER_URL = core.getInput('rancher_url', { required: true });
-  const RANCHER_ACCESS = core.getInput('rancher_access', { required: true });
-  const RANCHER_KEY = core.getInput('rancher_key', { required: true });
-  const PROJECT_ID = core.getInput('project_id', { required: true });
-  const STACK_NAME = core.getInput('stack_name', { required: true });
-  const SERVICE_NAME = core.getInput('service_name', { required: true });
-  const DOCKER_IMAGE = core.getInput('docker_image', { required: true });
- 
-  const rancherApi = request.defaults({
-    baseUrl: `${RANCHER_URL}/v2-beta/projects/${PROJECT_ID}`,
-    auth: {
-      user: RANCHER_ACCESS,
-      pass: RANCHER_KEY
+  const rancherUrl = core.getInput('rancher_url', {required: true});
+  const rancherToken = core.getInput('rancher_token', {required: true});
+  const clusterId = core.getInput('cluster_id', {required: true});
+  const projectId = core.getInput('project_id', {required: true});
+  const namespace = core.getInput('namespace', {required: true});
+  const deployment = core.getInput('deployment', {required: true});
+  const dockerImage = core.getInput('docker_image', {required: true});
+
+  await axios.patch(
+    `${rancherUrl}/k8s/clusters/${clusterId}/api/apps/v1/namespaces/${namespace}/deployments/${deployment}`,
+    [
+      {
+        op: 'replace',
+        path: '/spec/template/spec/containers/0/image',
+        value: dockerImage,
+      },
+    ],
+    {
+      headers: {
+        'Content-Type': 'application/json-patch+json',
+        'Authorization': 'Bearer ' + rancherToken,
+      },
     },
-    json: true
-  });
+  );
 
-  let success = false;
-  // Check the stack
-  const stack = await rancherApi.get(`/stacks?name=${STACK_NAME}`);
-  if (!stack || !stack.data[0]) {
-    throw new Error('Could not find stack name. Check the stack_name input. Deploy failed!');
-  }
-  const stackId = stack.data[0].id;
-
-  // Check the service
-  const service = await rancherApi.get(`/services?name=${SERVICE_NAME}&stackId=${stackId}`);
-  if (!service || !service.data[0]) {
-    throw new Error('Could not find service name. Check the service_name input. Deploy failed!');
-  }
-  const { id, launchConfig } = service.data[0];
-  launchConfig.imageUuid = `docker:${DOCKER_IMAGE}`;
-
-  // Upgrade
-  const body = {
-    inServiceStrategy: {
-      launchConfig
-    }
-  };
-  await rancherApi.post(`/service/${id}?action=upgrade`, { body });
-  console.log('Waiting for upgrade ...');
-  await waitForState('upgraded', rancherApi, id);
-
-  // Finish upgrade
-  await rancherApi.post(`/service/${id}?action=finishupgrade`);
-  console.log('Waiting for service starting ...');
-  await waitForState('active', rancherApi, id);
-
-  console.log('Service is running, upgrade successful');
-  core.setOutput('result', success);
+  await axios.post(
+    `${rancherUrl}/v3/project/${clusterId}:${projectId}/workloads/deployment:${namespace}:${deployment}?action=redeploy`,
+    {},
+    {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + rancherToken,
+      },
+    },
+  );
 }
 
 function handleError(err) {
