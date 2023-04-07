e79e5491f53d1f0a9b8d5ec1d4d5a7aeaeea2709
Adrian Kunz <clashsoft@hotmail.com>
2023-04-07 13:10:05 +0200

    style: Clean up main.js

--- main.js
+++ main.js
@@ -16,23 +16,23 @@ const waitForState = async (waitFor, http, baseUrl, id, retryCount, retryDelay) 
   if (retryCount === 0) {
     throw new Error(`Maximum retries exceeded waiting for state ${waitFor}`);
   }
-}
+};
 
 async function main() {
-  const RANCHER_URL = core.getInput('rancher_url', { required: true });
-  const RANCHER_ACCESS = core.getInput('rancher_access', { required: true });
-  const RANCHER_KEY = core.getInput('rancher_key', { required: true });
-  const PROJECT_ID = core.getInput('project_id', { required: true });
-  const STACK_NAME = core.getInput('stack_name', { required: true });
-  const SERVICE_NAME = core.getInput('service_name', { required: true });
-  const DOCKER_IMAGE = core.getInput('docker_image', { required: true });
+  const RANCHER_URL = core.getInput('rancher_url', {required: true});
+  const RANCHER_ACCESS = core.getInput('rancher_access', {required: true});
+  const RANCHER_KEY = core.getInput('rancher_key', {required: true});
+  const PROJECT_ID = core.getInput('project_id', {required: true});
+  const STACK_NAME = core.getInput('stack_name', {required: true});
+  const SERVICE_NAME = core.getInput('service_name', {required: true});
+  const DOCKER_IMAGE = core.getInput('docker_image', {required: true});
   const RETRY_COUNT = +core.getInput('retry_count');
   const RETRY_DELAY = +core.getInput('retry_delay');
 
   const http = new HttpClient('actions-rancher-deploy', undefined, {
     headers: {
-      Authorization: `Basic ${Buffer.from(`${RANCHER_ACCESS}:${RANCHER_KEY}`).toString('base64')}`
-    }
+      Authorization: `Basic ${Buffer.from(`${RANCHER_ACCESS}:${RANCHER_KEY}`).toString('base64')}`,
+    },
   });
   const baseUrl = `${RANCHER_URL}/v2-beta/projects/${PROJECT_ID}`;
 
@@ -49,14 +49,14 @@ async function main() {
   if (!service || !service.data[0]) {
     throw new Error('Could not find service name. Check the service_name input. Deploy failed!');
   }
-  const { id, launchConfig } = service.data[0];
+  const {id, launchConfig} = service.data[0];
   launchConfig.imageUuid = `docker:${DOCKER_IMAGE}`;
 
   // Upgrade
   await http.postJson(`${baseUrl}/service/${id}?action=upgrade`, {
     inServiceStrategy: {
-      launchConfig
-    }
+      launchConfig,
+    },
   });
   console.log('Waiting for upgrade ...');
   await waitForState('upgraded', http, id, RETRY_COUNT, RETRY_DELAY);
