7ef29b2172c06263b6149d1042f2497406cb9f5f
Adrian Kunz <a.kunz@uni-kassel.de>
2024-08-26 15:54:10 +0200

    feat: Allow specifying workload kind

--- main.ts
+++ main.ts
@@ -10,9 +10,21 @@ async function main() {
   const clusterId = core.getInput('cluster_id', {required: true});
   const projectId = core.getInput('project_id', {required: true});
   const namespace = core.getInput('namespace', {required: true});
-  const deployment = core.getInput('deployment', {required: true});
+  const kind = core.getInput('kind', {required: false})?.toLowerCase() || 'deployment';
+  let workload = core.getInput('workload', {required: false});
+  const deployment = core.getInput('deployment', {required: false});
   const dockerImage = core.getInput('docker_image', {required: true});
-  const containerId = core.getInput('container_id', {required: false}) || 0;
+  const containerId = core.getInput('container_id', {required: false}) || '0';
+
+  if (!workload) {
+    if (!deployment) {
+      throw new Error('Either workload or deployment must be provided');
+    }
+    workload = deployment;
+    console.warn(`The 'deployment' argument is deprecated, please use \`workload: ${workload}\` instead`);
+  }
+
+  const path = getContainerImagePath(kind, containerId);
 
   const http = new HttpClient('actions-rancher-update', undefined, {
     headers: {
@@ -21,11 +33,11 @@ async function main() {
   });
 
   await http.patchJson(
-    `${rancherUrl}/k8s/clusters/${clusterId}/apis/apps/v1/namespaces/${namespace}/deployments/${deployment}`,
+    `${rancherUrl}/k8s/clusters/${clusterId}/apis/apps/v1/namespaces/${namespace}/${kind}/${workload}`,
     [
       {
         op: 'replace',
-        path: `/spec/template/spec/containers/${containerId}/image`,
+        path,
         value: dockerImage,
       },
     ],
@@ -35,9 +47,26 @@ async function main() {
   );
 
   await http.postJson(
-    `${rancherUrl}/v3/projects/${clusterId}:${projectId}/workloads/deployment:${namespace}:${deployment}?action=redeploy`,
+    `${rancherUrl}/v3/projects/${clusterId}:${projectId}/workloads/${kind}:${namespace}:${deployment}?action=redeploy`,
     {},
   );
+}
+
+function getContainerImagePath(kind: string, containerId: string) {
+  switch (kind) {
+    // https://kubernetes.io/docs/concepts/workloads/controllers/
+    case 'deployment':
+    case 'statefulset':
+    case 'daemonset':
+    case 'replicaset':
+    case 'replicationcontroller':
+    case 'job':
+      return `/spec/template/spec/containers/${containerId}/image`;
+    case 'cronjob':
+      return `/spec/jobTemplate/spec/template/spec/containers/${containerId}/image`;
+    default:
+      throw new Error(`Unsupported workload kind: ${kind}`);
+  }
 }
 
 function handleError(err: Error) {
